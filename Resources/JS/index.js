7c9e7a0af0f19f19e3e3cd0bdce38bf13381bba4
Rizal Suhaimi <105201048+incursio16@users.noreply.github.com>
2024-10-24 19:51:13 +0800

    work on mobile nav bar - in progress

--- Resources/JS/index.js
+++ Resources/JS/index.js
@@ -2,3 +2,5 @@ document.getElementById("contactForm").onsubmit = function(event) {
     event.preventDefault();
     alert("This is still in development. Your message was not sent");
 };
+
+document.getElementsByClassName
