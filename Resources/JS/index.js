b6adf887941798cbf1e4493ae9fc95e0b01f4e72
Rizal Suhaimi <105201048+incursio16@users.noreply.github.com>
2024-10-25 11:01:36 +0800

    add navigation dropdown

--- Resources/JS/index.js
+++ Resources/JS/index.js
@@ -3,4 +3,22 @@ document.getElementById("contactForm").onsubmit = function(event) {
     alert("This is still in development. Your message was not sent");
 };
 
-document.getElementsByClassName
+const dropdownToggle = document.getElementById("dropdown-toggle");
+const dropdownContent = document.getElementById("dropdown-content");
+
+// Add click event to toggle the dropdown
+dropdownToggle.addEventListener("click", event => {
+    event.preventDefault(); // Prevent default anchor behaviour
+
+    // Toggle the "show" class to either display or hide the dropdown
+    dropdownContent.classList.toggle("show");
+});
+
+// Close the dropdown if clicked outside
+window.addEventListener("click", event => {
+    if (!event.target.closest("#dropdown-toggle")) {
+        if (dropdownContent.classList.contains("show")) {
+            dropdownContent.classList.remove("show");
+        }
+    }
+});
